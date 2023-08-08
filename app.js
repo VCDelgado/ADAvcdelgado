581059e4550cf6f91b9382a9438dc69caaea6f66
VCDelgado <valeriacdelgado@gmail.com>
2023-08-08 15:57:54 -0300

    se agrego paginado y numero de pagina

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
+let $totalPersonajes = document.getElementById ("totalPersonajes")
 let $cuadro = document.getElementById ("cuadro")
 let todosPersonajes = [];
-let $siguienteB = document.getElementById ("siguiente");
-let $anteriorB = document.getElementById ("anterior")
+let $verMas = document.getElementById ("verMas")
+
 let pagina = 1;
-let $mujerB = document.getElementById("mujer")
+
+let $numeroPagina = document.getElementById ("numeroPagina");
+let $totalPaginas = document.getElementById ("totalPaginas")
+let $primerPaginaB = document.getElementById ("primerPagina");
+let $siguienteB = document.getElementById ("siguiente");
+let $anteriorB = document.getElementById ("anterior");
+let $ultimaPaginaB = document.getElementById ("ultimaPagina");
+
+let $todosB = document.getElementById("todos");
+let $mujerB = document.getElementById("mujer");
+let $hombreB = document.getElementById("hombre");
+let $sinGeneroB = document.getElementById("sinGenero");
+let $desconocidoB = document.getElementById("desconocido");
+
+// FETCH
+function usarFetch (numeroPagina) {
+    respuesta = fetch (`https://rickandmortyapi.com/api/character/?page=${numeroPagina}`);
+    respuesta.then((data)=> {
+    return data.json ()
+    
+})
+.then ((data)=> {
+    resultados = data.results;
+    informacion = data.info;
+    cuadro (resultados);
+    $numeroPagina.innerHTML = numeroPagina;
+    $totalPaginas.innerHTML = informacion.pages;
+    }
+)
+
+.catch ((error) => {
+    console.log (error)
+});
+};
+
+usarFetch (pagina);
+
+$numeroPagina.innerHTML = `<p> Pagina actual = ${pagina}</p>`;
+
+
+// TODOS LOS PERSONAJES
 
 function cuadro (resultados) {
     $cuadro.innerHTML = "";
-    for (let i=0; i<resultados.length; i++) {
+    $totalPersonajes.innerHTML = resultados.length;
+        for (let i=0; i<resultados.length; i++) {
         $cuadro.innerHTML +=  `<div class="cuadrodatos">
                                 <img src=${resultados[i].image} alt="">
                                 <p> <span class="detalle"> Nombre: </span> ${resultados[i].name}</p>
@@ -16,30 +58,25 @@ function cuadro (resultados) {
                                 <p><span class="detalle"> Estado: </span> ${resultados[i].status}</p>
                                 <p><span class="detalle"> Origen: </span> ${resultados[i].origin.name}</p>
                                 <p><span class="detalle"> Locación: </span> ${resultados[i].location.name}</p>
-                            </div>`
+                                <a href="#"> <span class="verMas">Ver mas...</span> ${resultados[i].id}</a> 
+                                </div>`                                                            
     }
     }
+
+
+
+
+
     
 
 
-function usarFetch (numeroPagina) {
-    let respuesta = fetch (`https://rickandmortyapi.com/api/character/?page=${numeroPagina}`);
-    respuesta.then((data)=> {
-    return data.json ()
-})
-.then ((data)=> {
-    let resultados = data.results;
-    cuadro (resultados);
-    }
-)
+// FILTROS POR GENERO
 
-
-.catch ((error) => {
-    console.log (error)
-});
-};
-
-usarFetch (pagina);
+function filtrarTodos () {
+    let todos = resultados;
+    cuadro (todos);
+}
+$todosB.addEventListener ("click",filtrarTodos);
 
 
 function filtrarMujeres () {
@@ -50,18 +87,58 @@ function filtrarMujeres () {
 }
 $mujerB.addEventListener ("click",filtrarMujeres);
 
+function filtrarHombres () {
+    let hombres = resultados.filter ((personaje) => {
+        return personaje.gender === "Male";
+    })
+    cuadro (hombres);
+}
+$hombreB.addEventListener ("click",filtrarHombres);
+
+function filtrarSinGenero () {
+    let sinGenero = resultados.filter ((personaje) => {
+        return personaje.gender === "genderless";
+    })
+    cuadro (sinGenero);
+}
+$sinGeneroB.addEventListener ("click",filtrarSinGenero);
+
+function filtrarDesconocido () {
+    let desconocido = resultados.filter ((personaje) => {
+        return personaje.gender === "unknown";
+    })
+    cuadro (desconocido);
+}
+$desconocidoB.addEventListener ("click",filtrarDesconocido);
+
+
+
+// PAGINACION
+
+function primerPagina () {
+    if (pagina > 1) {
+        pagina = 1;
+        usarFetch (1);
+    }    
+};
+$primerPaginaB.addEventListener ("click",primerPagina);
+
 function siguientePagina () {
-    pagina++
+    pagina+=1;
     usarFetch (pagina)
 };
-
-
 $siguienteB.addEventListener ("click",siguientePagina);
 
 function anteriorPagina () {
-    pagina--
+    pagina-= 1;
     usarFetch (pagina)
 };
-
-
 $anteriorB.addEventListener ("click",anteriorPagina);
+
+function ultimaPagina () {
+    if (pagina < informacion.pages) {
+        pagina = informacion.pages
+        usarFetch (pagina);
+    }
+};
+$ultimaPaginaB.addEventListener ("click",ultimaPagina);
